ad4e50f46ce402497eadf7b7c7e8bed6776d4c3f
Golfredo Perez Fernandez <golfredo.pf@gmail.com>
2023-08-26 17:21:02 -0400

    asd

--- src/pages/success.js
+++ src/pages/success.js
@@ -11,23 +11,15 @@ import Alert from '@mui/material/Alert';
 import { useCallback,useRef, useMemo, useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { loadStripe } from "@stripe/stripe-js";
+const stripe = require('stripe')("pk_test_51NV05cGc5cz7uc72xTzSNZNeg3dsIWX9hZo4Y7nZH5WnFF8nuEJJwhSGviE29JHXzm8zovxToQDDVjLzfND57MWj00NdjCWocu");
 
 const now = new Date();
 const Page = () => {
   const router=useRouter();
   const sessionId=useRouter().query.session_id ;
   
-let stripePromise;
-const getStripe = () => {
-  if (!stripePromise) {
-    stripePromise = loadStripe("pk_test_51NV05cGc5cz7uc72xTzSNZNeg3dsIWX9hZo4Y7nZH5WnFF8nuEJJwhSGviE29JHXzm8zovxToQDDVjLzfND57MWj00NdjCWocu");
-  }
-
-  return stripePromise;
-};
 async function fecthstripe(){
   console.log("sessionId "+sessionId)
-  const stripe = await getStripe();
   if(sessionId){
     
     const session = await stripe.checkout.sessions.retrieve(sessionId);
